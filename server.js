12a770026efbc67c1a9a6c1e1f89961bd4140e85
Vishesh <vishesh666@live.com>
2018-12-06 02:09:43 +0800

    UI fix

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var http = require('http').Server(app)
 const fs = require('fs')
 const path = require('path')
 const io = require('socket.io')(http)
+const cleverbot = require("cleverbot.io")
+bot = new cleverbot("XGtoopiQvJ1XEGiM", "yU9dy1YLtWTtCJOMs3BZyQDq6aoAGgyM");
+bot.setNick("Nubo")
+bot.create(function (err, Nubo) {
+});
 const port = process.env.PORT || 3000
 const indexHTML = (() => {
     return fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8')
@@ -10,14 +15,29 @@ const indexHTML = (() => {
 
 const botHTML = (() => {
     return fs.readFileSync(path.resolve(__dirname, './bot.html'), 'utf-8')
-  })()
+	})()
+
+const talkHTML = (() => {
+    return fs.readFileSync(path.resolve(__dirname, './talk.html'), 'utf-8')
+  })()	
 
 app.get('/', function(req, res){
 	res.send(indexHTML)
 });
 
+app.get('/talk', function(req, res){
+	res.send(talkHTML)
+});
+
 app.get('/bot', function(req, res){
 	res.send(botHTML)
+});
+
+app.get('/answer', function(req, res){
+	bot.ask(req.query.q, function (err, response) {
+		res.send(response)
+		console.log(req.query.q + ' ' + response);
+	});
 });
 
 app.get('/say', function(req, res){
@@ -34,14 +54,8 @@ io.on('connection', (socket) => {
 	socket.on('disconnect', () => {
 		console.log('disconnected')
 	})
-	// socket.on('here', (data) => {
-	// 	console.log(data)
-	// 	socket.broadcast.emit('DDDD', (data))
-	// 	// io.emit('DDDD', (data))
-	// })
 	socket.on('publish', (data) => {
 		console.log(data)
 		socket.broadcast.emit('listen', (data))
-		// io.emit('DDDD', (data))
 	})
 })
